325f85e45a928fd9f248d53d7f725740ad09346c
christus-rex <marv875@gmail.com>
2026-08-14 06:59:07 -0400

    release: align service worker with v5.2.3

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = 'temple-maat-pwa-v5.3.1-manual-entry-2026-08-14';
+const VERSION = 'temple-maat-pwa-v5.2.3-manual-entry-2026-08-14';
 const STATIC_CACHE = `${VERSION}-static`;
 const RUNTIME_CACHE = `${VERSION}-runtime`;
 const CORE_ASSETS = [
