7697356ebd3f7fe023151441c1a88aac83c05cac
christus-rex <marv875@gmail.com>
2026-08-14 09:11:34 -0400

    pwa: bump cache namespace for v5.2.7 stewardship

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = 'temple-maat-pwa-v5.2.6-shem-dossiers-2026-08-14';
+const VERSION = 'temple-maat-pwa-v5.2.7-stewardship-governance-2026-08-14';
 const STATIC_CACHE = `${VERSION}-static`;
 const RUNTIME_CACHE = `${VERSION}-runtime`;
 const CORE_ASSETS = [
